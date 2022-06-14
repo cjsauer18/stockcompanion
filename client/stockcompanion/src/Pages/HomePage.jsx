bb90554ac9da9b22e1fb8a20241e488bbd28ac40
= <cjsauer18@gmail.com>
2022-06-14 14:59:32 -0400

    added stock library and features

--- client/stockcompanion/src/Pages/HomePage.jsx
+++ client/stockcompanion/src/Pages/HomePage.jsx
@@ -4,12 +4,15 @@
 import React, { useContext } from "react";
 import StockChart from "./StockChart";
 import { LoginContext } from "../Contexts/LoginContext";
+import CandlestickChart from "../views/financial charts/Candlestick Chart";
 
 function HomePage(props) {
   const { chartData } = useContext(LoginContext);
   return (
     <div>
-      <StockChart />
+      {/* <StockChart /> */}
+      <CandlestickChart />
+      {console.log("heynow")}
     </div>
   );
 }
